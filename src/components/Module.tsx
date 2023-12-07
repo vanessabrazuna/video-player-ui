7568bd07aa1707f36046063b0c168bba740495b0
vanessabrazuna <brazuna.nessa@gmail.com>
2023-12-07 15:37:17 -0300

    Using Async Thunks and creating a loading interface

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -1,8 +1,7 @@
-import * as Collapsible from "@radix-ui/react-collapsible"
 import { ChevronDown } from "lucide-react"
+import * as Collapsible from "@radix-ui/react-collapsible"
 import { Lesson } from "./Lesson"
-import { useAppSelector } from "../store"
-import { useDispatch } from "react-redux"
+import { useAppDispatch, useAppSelector } from "../store"
 import { play } from "../store/slices/player"
 
 interface ModuleProps {
@@ -12,7 +11,7 @@ interface ModuleProps {
 }
 
 export function Module({ title, amountOfLessons, moduleIndex }: ModuleProps) {
-  const dispatch = useDispatch()
+  const dispatch = useAppDispatch()
 
   const { currentModuleIndex, currentLessonIndex } = useAppSelector(state => {
     const { currentModuleIndex, currentLessonIndex } = state.player
@@ -21,7 +20,7 @@ export function Module({ title, amountOfLessons, moduleIndex }: ModuleProps) {
   })
 
   const lessons = useAppSelector((state) => {
-    return state.player.course.modules[moduleIndex].lessons
+    return state.player.course?.modules[moduleIndex].lessons
   })
 
   return (
@@ -41,7 +40,7 @@ export function Module({ title, amountOfLessons, moduleIndex }: ModuleProps) {
 
       <Collapsible.Content>
         <nav className="relative flex flex-col gap-4 p-6">
-          {lessons.map((lesson, lessonIndex) => {
+          {lessons && lessons.map((lesson, lessonIndex) => {
             const isCurrent = currentModuleIndex === moduleIndex && 
               currentLessonIndex === lessonIndex
 
